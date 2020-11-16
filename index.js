a1416de7c2be3d12a1de1e04588f64cda6a91a8f
t8hosa01 <46774951+t8hosa01@users.noreply.github.com>
2020-11-16 15:06:09 +0200

    queue

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const queue = new Map();
+
 const ytdl = require('ytdl-core');
 
 const fs = require('fs');
@@ -22,43 +24,81 @@ client.on("message", async (message) => {
     if(!message.content.startsWith(prefix)) return;
 
     const args = message.content.substring(prefix.length).split(" ");
+    const serverQueue = queue.get(message.guild.id);
 
     if(message.content.startsWith(`${prefix}perjantai`)) {
         const voiceChannel = message.member.voice.channel;
         if(!voiceChannel) return message.channel.send("Et oo voice channelilla");
 
+        //const songInfo = await ytdl.getInfo(args[1]);
+        const song = "./biisi.mp3"
+
         if (talkedRecently.has(message.author.id)) {
             message.channel.send('Sul on cooldown');
         } else {
-            try {
-                var connection = await voiceChannel.join();
-            } catch (error) {
-                console.log(error);
-            }
-    
-            const dispatcher = connection.play('./biisi.mp3')
-            .on('finish', () => {
-                voiceChannel.leave();
-            })
-            .on('error', error => {
-                console.log(error);
-            })
-            dispatcher.setVolumeLogarithmic(5 / 5);
 
-            talkedRecently.add(message.author.id);
-            setTimeout(() => {
-                talkedRecently.delete(message.author.id);
-            }, 60000);
+            if(!serverQueue) {
+                const queueConstruct = {
+                    textChannel: message.channel,
+                    voiceChannel: voiceChannel,
+                    connection: null,
+                    songs: [],
+                    volume: 5,
+                    playing: true
+                }
+                queue.set(message.guild.id, queueConstruct);
+                queueConstruct.songs.push(song)
+
+                try {
+                    var connection = await voiceChannel.join();
+                    queueConstruct.connection = connection;
+                    play(message.guild, queueConstruct.songs[0]);
+                } catch (error) {
+                    queue.delete(message.guild.id);
+                    console.log(error);
+                }
+            } else {
+                serverQueue.songs.push(song);
+                return message.channel.send('Perjantai-biisi lisätty queueen');
+            }
+            return undefined;
         }
 
     } else if (message.content.startsWith(`${prefix}lopeta`)) {
         if(!message.member.voice.channel) return message.channel.send('Sun pitää olla voice kannul että voit stoppaa musan');
+        serverQueue.songs = [];
+        serverQueue.connection.dispatcher.end();
         message.member.voice.channel.leave();
         return undefined;
     }
 
     if(message.content.startsWith(`${prefix}entajuu`)) {
         message.channel.send('https://static.luolasto.org/file/varavarasto/38215/tiedosto.jpg')
+    }
+
+    function play(guild, song) {
+        const serverQueue = queue.set(guild.id);
+    
+        if(!song) {
+            serverQueue.voiceChannel.leave();
+            queue.delete(guild.id);
+            return;
+        }
+    
+        const dispatcher = serverQueue.connection.play(song)
+            .on('finish', () => {
+                serverQueue.songs.shift();
+                play(guild, serverQueue.songs[0]);
+            })
+            .on('error', error => {
+                console.log(error);
+            })
+            dispatcher.setVolumeLogarithmic(5 / 5);
+    
+            talkedRecently.add(message.author.id);
+            setTimeout(() => {
+                talkedRecently.delete(message.author.id);
+            }, 60000);
     }
  
 });
